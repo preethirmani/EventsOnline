60df72d897e778fd5a110277f2495383a7941956
Preethi <writetopreethir@gmail.com>
2022-11-25 00:43:16 -0800

    Adding JSON for Events and dependecies

---
+++ frontend/src/programs.js
@@ -0,0 +1,119 @@
+const programs = [
+  {
+    _id: 1,
+    name:'Taichi',
+    image: '/images/taichi.jpg',
+    description:'Begin your Taichi journey: Traditional Movements for Health/Rejuvenation. ',
+    host:'Quality of Life',
+    date:'Sun, Nov 27, 2022',
+    time:'11:00 AM EST',
+    location:'Online',
+    price:'Free',
+    rating:'4.5',
+    numReviews:'170',
+    seatsAvailable:'',
+    category:'Health',
+    requirements:'',
+    about:'',
+    duration: '30 minutes'
+  },
+  {
+    _id: 2,
+    name:'Decentralized Dance Party ',
+    image: '/images/danceParty.jpg',
+    description:'The Decentralized Dance Party is an Open-Source Party Movement on a mission to unite the world in Celebration and win the Nobel Peace Prize for Partying ',
+    host:'Decentralized Dance Party',
+    date:'Sat, Dec 24, 2022',
+    time:'5:00 PM EST',
+    location:'Online',
+    price:'Free',
+    rating:'4.5',
+    numReviews:'170',
+    seatsAvailable:'',
+    category:'Health',
+    requirements:'',
+    about:'',
+    duration:'30 minutes'
+  },
+
+  {
+   _id: 3,
+    name:'SocietyX Gengle Yoga Flow',
+    image: '/images/yoga.jpg',
+    description:'Gentle yoga is a form of yoga that moves at a slower more meditative pace. We will relax and focus on breathing techniques to help destress our bodies. ',
+    host:'The SocietyX Community',
+    date:'Sat, Dec 3, 2022',
+    time:'11:30 AM EST',
+    location:'Online',
+    price:'Free',
+    rating:'4.5',
+    numReviews:'170',
+    seatsAvailable:'',
+    category:'Health',
+    requirements:'',
+    about:'',
+    duration:'60 minutes'
+  },
+
+  {
+    _id: 4,
+    name:'Online Dance Party ',
+    image: '/images/danceOnZoom.jpg',
+    description:'Online Dance Party - Free on Zoom',
+    host:'Cynthia - Host/DJ',
+    date:'All Fridays from Nov 5,2022',
+    time:'7:00 PM PST',
+    location:'Online',
+    price:'Free',
+    rating:'4.4',
+    numReviews:'430',
+    seatsAvailable:'',
+    category:'Dance',
+    requirements:'Sold Out',
+    about:`Do you want to party, but don't really feel like going out? Tonight come to a fabulous Online Nightclub where the VIP section is always reserved just for YOU. Unlimited drinks are on the house, there's no cover charge and you don't have to pay for parking, cabs, or baby sitters.`,
+    duration:'30 minutes'
+  },
+
+  {
+   _id: 5,
+    name:'Mindfulness, Music & Deep Listening',
+    image: '/images/meditation.jpg',
+    description:`Guided meditations in stillness and movement offer space to slow down, rest, revive and become fully present. Deep Listening practices by Pauline Oliveros and others helps us ground ourselves in our listening body and in our connection with the Earth. We sound and create together, using our voices and instruments you bring in ways that feel natural and spontaneous.
+
+      You are invited to engage as much or as little as your energy allows - resting and listening are always options, too. `,
+    host:'Susanne Olbrich and Joe Holtaway',
+    date:'Once in a month starting 19 Dec',
+    time:'9:00 PM PST',
+    location:'Online',
+    price:'45',
+    rating:'4.7',
+    numReviews:'115',
+    seatsAvailable:'',
+    category:'Health',
+    requirements:'',
+    about:'',
+    duration:'60 minutes'
+  },
+
+  {
+    _id: 6,
+    name:'Decentralized Dance Party ',
+    image: '/images/danceParty.jpg',
+    description:'The Decentralized Dance Party is an Open-Source Party Movement on a mission to unite the world in Celebration and win the Nobel Peace Prize for Partying ',
+    host:'Decentralized Dance Party',
+    date:'Sat, Dec 24, 2022',
+    time:'5:00 PM EST',
+    location:'Online',
+    price:'Free',
+    rating:'4.5',
+    numReviews:'170',
+    seatsAvailable:'',
+    category:'Health',
+    requirements:'',
+    about:'',
+    duration:'30 minutes'
+  }
+
+];
+
+export default programs;
