36bd560d263270043967442a796830ec40b5e99d
maryl-in32 <115038241+maryl-in32@users.noreply.github.com>
2022-12-04 01:12:39 -0800

    Styled header and footer

--- frontend/src/components /Footer.js
+++ frontend/src/components /Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy;EventsOnline</Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
--- frontend/src/components /Header.js
+++ frontend/src/components /Header.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg"
+      collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">EventsOnline</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="ms-auto">
+              <Nav.Link href="/create"><i className='fas fa-calendar-plus'></i> Create Event</Nav.Link>
+              <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+              <Nav.Link href="/sign up"><i className='fas fa-pen-to-square'></i> Sign Up</Nav.Link>
+            </Nav>
+        </Container>
+      </Navbar>
+
+    </header>
   )
 }
 
