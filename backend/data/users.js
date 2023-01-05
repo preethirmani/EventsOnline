25a2e2e45a59ef521b3daafade4a3d3ef7e4222d
Preethi <writetopreethir@gmail.com>
2023-01-05 15:55:50 -0800

    Sample data created

---
+++ backend/data/users.js
@@ -0,0 +1,31 @@
+import bcryptjs from 'bcryptjs';
+
+
+const users = [
+  {
+    name : 'Admin User',
+    email : 'adminUser@test.com',
+    password: bcryptjs.hashSync('12134', 10),
+    isAdmin: true
+  },
+  {
+    name : 'John Doe',
+    email : 'john.doe@test.com',
+    password: bcryptjs.hashSync('1234',10),
+    isAdmin: false
+  },
+  {
+    name : 'Jane Doe',
+    email : 'jane.doe@test.com',
+    password: bcryptjs.hashSync('1234',10),
+    isAdmin: false
+  },
+  {
+    name : 'Jake Doe',
+    email : 'jake.doe@test.com',
+    password: bcryptjs.hashSync('1234',10),
+    isAdmin: false
+  }
+];
+
+export default users;
